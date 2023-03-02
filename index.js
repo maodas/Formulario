80093a48f3d5de526ffabee0ab80bc00b27cafa1
maodas <rodas807@gmail.com>
2023-03-01 22:20:49 -0600

    .

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 var formulario = document.getElementById('form-login')
+var pass = document.getElementById('pass-login')
+
+var expMay = RegExp("[A-Z]")
+var expMin = RegExp("[a-z]")
+var expNum = RegExp("[0-9]")
 
 formulario.addEventListener('submit',function(evento){
     evento.preventDefault()
-    alert("Bienvenido " +document.getElementById('nombre-login').value)
+
+    var error =""
+    if(!pass.value.match(expMay)){
+        error +=" Debe tener una mayuscula "
+    }if(!pass.value.match(expMin)){
+        error +=" Debe tener una minuscula "
+    }if(!pass.value.match(expNum)){
+        error +=" Debe tener un numero "
+    }else{
+        error = "Bienvenido"
+    }
+    alert(error)
+    
 })
