f85601fa05aec04f75b70ab357907bf537420da6
maodas <rodas807@gmail.com>
2023-03-02 22:30:05 -0600

    .

--- main.js
+++ main.js
@@ -13,8 +13,26 @@ function createWindow(){
     });
     ventana.loadFile('index.html')
 }
+
+let ventana2;
+
+function createWindow2(){
+    ventana2 = new BrowserWindow({
+        height:300,
+        width:300,
+        webPreferences:{
+            preload: path.join(app.getAppPath(),'preload.js')
+        }
+    });
+    ventana2.loadFile('segundo.html')
+}
+
 ipcMain.on('registroValido',function(event,args){
     console.log(args)
-    ventana.webContents.send('inicioCorrecto','Bienvenido')
+    createWindow2()
+    ventana2.webContents.on('did-finish-load',function(){
+        ventana2.webContents.send('inicioCorrecto','Bienvenido')
+    })
+        
 })
 app.whenReady().then(createWindow)
