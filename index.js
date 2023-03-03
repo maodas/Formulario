2038f06ba77051826d07cece30581b93061b900a
maodas <rodas807@gmail.com>
2023-03-03 02:06:08 -0600

    .

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ var expMay = RegExp("[A-Z]")
 var expMin = RegExp("[a-z]")
 var expNum = RegExp("[0-9]")
 
-var expPass= RegExp("(?=.*[a-z])(?=.*[A-Z](?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}")
-
 formulario.addEventListener('submit',function(evento){
     evento.preventDefault()
 
